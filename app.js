0eab99b614976fce08df916c15d3f7a6c4df5fff
joshasgard <idowuodesanmi@gmail.com>
2022-03-22 20:42:36 +0100

    NodeJS Tutorial Initial Commit

---
+++ app.js
@@ -0,0 +1,53 @@
+// //require('./module')
+// const {readFileSync, writeFileSync} = require('fs');
+// // or const fs = require('fs');
+// const first = readFileSync('./content/first.txt', 'utf-8')
+// const second = readFileSync('./content/second.txt', 'utf-8')
+
+// writeFileSync('./content/result-sync.txt', 
+//                 `Here is the result: ${first}, ${second}`, {flag:'a'})
+
+// const {readFile, writeFile} = require('fs');
+// console.log('start')
+// readFile('./content/first.txt','utf8', (err, result)=>{
+//     if (err){
+//         console.log(err)
+//         return
+//     }
+//     const first = result;
+//     readFile('./content/second.txt', 'utf-8',(err,result)=>{
+//         if (err){
+//             console.log(err)
+//             return
+//         }
+//         const second = result;
+//         writeFile('./content/result-async.txt',
+//         `Here is the result:${first}, ${second}`,
+//         (err,result)=>{
+//             if (err){
+//                 console.log(err);
+//                 return;
+//             }
+//             console.log('done with this task');
+//         })
+//     })
+// })
+// console.log('starting next task')
+
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+    if (req.url === '/'){
+        res.end('Welcome to homepage')
+    }
+    if (req.url === '/about'){
+        res.end('Here is our short history')
+    }
+    res.end(`
+        <h1>Oops!</h1>
+        <p>We can't find the requested page</p>
+        <a href="/">Go back to the homepage</a>
+    `)
+})
+
+server.listen(5000)
---
+++ module.js
@@ -0,0 +1,43 @@
+// // const num1 = 5
+// // const num2 = 10
+// // function addValues() {
+// //     console.log(`the sum is : ${num1 + num2}`)
+// // }
+// // addValues()
+
+// // Globals
+
+// // Built-In Base Modules
+// // 1. OS
+// // 2. PATH
+// // 3. FS - File System
+// // 4. HTTP module
+// // 
+
+// // const os = require('os')
+
+// // // info about current user and system uptime
+// // const user = os.userInfo()
+// // console.log(user)
+// // console.log(`System uptime is ${os.uptime()} seconds`)
+
+
+// // const currentOS = {
+// //     name: os.type(),
+// //     release: os.release(),
+// //     totalMem: os.totalmem(),
+// //     freeMem: os.freemem()
+// // }
+// // console.log(currentOS)
+// const path = require('path')
+// console.log(path.sep)
+
+// const filePath = path.join('/content','subfolder', 'test.txt')
+// console.log(filePath)
+
+// const base = path.basename(filePath)
+// console.log(base)
+
+// const absolute = path.resolve(__dirname,'content', 'subfolder','test.txt')
+// console.log(absolute)
+//Sync Vs Async
