7cbc35c7a148dbe4339fe512ceea4a549f70001d
joshasgard <idowuodesanmi@gmail.com>
2022-03-25 16:44:47 +0100

    Node Express Fundamentals

--- app.js
+++ app.js
@@ -117,25 +117,105 @@
 //  3. Duplex
 //  4. Transform
 
-const express = require('express');
-const path = require('path');
-const app = express();
+// const express = require('express');
+// const path = require('path');
+// const app = express();
 
-// setup static and middleware
-app.use(express.static('./public'))
+// // setup static and middleware
+// app.use(express.static('./public'))
+
+// // app.get('/', (req,res)=>{
+// //     res.sendFile(path.resolve(__dirname, './facebook-Log/index.html'))
+// // })
+// // adding to static assets
+// // SSR
+
+// app.all('*',(req,res)=>{
+//     res.status(404).send('resource not found')
+// })
+
+// app.listen(5000, ()=>{
+//     console.log('Server is listening on Port 5000...')
+// })
+
+
+// const express = require('express');
+// const app = express();
+// const { products } = require('./data')
 
 // app.get('/', (req,res)=>{
-//     res.sendFile(path.resolve(__dirname, './facebook-Log/index.html'))
+//     res.send('<h1>Home Page</h1> <a href="/api/products">products</a>')
 // })
-// adding to static assets
-// SSR
 
-app.all('*',(req,res)=>{
-    res.status(404).send('resource not found')
+// app.get('/api/products/:productID', (req,res)=>{
+//     // console.log(req);
+//     // console.log(req.params);
+//     const {productID} = req.params;
+//     const singleProduct = products.find((product) => product.id === Number(productID));
+//     if (!singleProduct){
+//         return res.status(404).send('Product does not exist')
+//     }
+//     res.json(singleProduct);
+//     // // const newProducts = products.map((product)=>{
+//     // //     const {id,name,image} = product;
+//     // //     return {id,name,image}
+//     // })
+//     // res.json(newProducts)
+// })
+
+// app.get('/api/products/:productID/review/:reviewID', (req,res)=>{
+//     console.log(req.params);
+//     res.send('hello world');
+// })
+
+// app.get('/api/v1/query', (req,res)=>{
+//     console.log(req.query);
+//     const {search, limit} = req.query
+//     let sortedProducts = [...products];
+    
+//     if (search){
+//         sortedProducts = sortedProducts.filter((product)=>{
+//             return product.name.startsWith(search)
+//         })
+//     }
+//     if (limit){
+//         sortedProducts = sortedProducts.slice(0,Number(limit))
+//     }
+//     if (sortedProducts.length < 1){
+//         return res.status(200).json({success:true,data:[]})
+//     }
+//     res.status(200).json(sortedProducts)
+//     //res.send('hello world')
+// })
+
+// app.listen(5000,()=>{
+//     console.log('Server is listening on port 5000......')
+// })
+
+const express = require('express')
+const app = express()
+const logger = require('./logger')
+const authorize = require('./authorize')
+// req => middleware => res
+
+app.use(express.static('./public'))
+
+app.get('/', (req,res) => {
+    res.send('Home')
 })
 
-app.listen(5000, ()=>{
-    console.log('Server is listening on Port 5000...')
+app.get('/about', (req,res)=>{
+    res.send('About')
 })
 
+app.get('/api/products', (req,res)=>{
+    res.send('Product')
+})
 
+app.get('/api/items', (req,res)=>{
+    res.send('Items')
+})
+
+app.listen(5000, ()=> {
+    console.log('Server is listening on port 5050...')
+})
