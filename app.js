04fb7efb9057015b60513da2f6b4a4b5a1088eb1
joshasgard <idowuodesanmi@gmail.com>
2022-03-25 13:13:38 +0100

    Static file serving with html

--- app.js
+++ app.js
@@ -124,8 +124,14 @@ const app = express();
 // setup static and middleware
 app.use(express.static('./public'))
 
-app.get('/', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, './facebook-Log/index.html'))
+// app.get('/', (req,res)=>{
+//     res.sendFile(path.resolve(__dirname, './facebook-Log/index.html'))
+// })
+// adding to static assets
+// SSR
+
+app.all('*',(req,res)=>{
+    res.status(404).send('resource not found')
 })
 
 app.listen(5000, ()=>{
